fe58973c1f5a6c2d967fdd65543c575c0c0cdac4
Uladzislau Radzivil <radzivil.work@gmail.com>
2024-08-16 12:40:50 +0200

    done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.slice(i, i + 10));
+    }
+
+    let morse = [];
+    for(let i = 0; i < arr.length; i++) {
+        let x = '';
+        for(let j = 0; j < arr[i].length; j += 2) {
+            if(arr[i][j] + arr[i][j + 1] == 10) {
+                x += '.';
+            }
+            if(arr[i][j] + arr[i][j + 1] == 11) {
+                x += "-";
+            }
+            if(arr[i][j] + arr[i][j + 1] == '**') {
+                x = '**';
+            }
+        }
+        morse.push(x);
+    }
+    let result = '';
+    for (let i = 0; i < morse.length; i++){
+        if(morse[i] == '**') {
+            result += ' ';
+        } else {
+            result += MORSE_TABLE[morse[i]]
+        }
+    }
+    return result;
 }
 
 module.exports = {
